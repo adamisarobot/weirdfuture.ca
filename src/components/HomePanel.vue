ecf012cff57359bad259091a67aed1ad8e302d58
Adam4ever <adam4ever@gmail.com>
2023-06-07 06:48:41 -0300

    Update colours
    - Added panel colours as props
    - Add TallDeco font (there is an error in the console
    - Add Speech bubble component

--- src/components/HomePanel.vue
+++ src/components/HomePanel.vue
@@ -1,14 +1,37 @@
 <template>
   <div
-    class="panel relative m-[1vmin] inline-block h-52 flex-auto basis-52 overflow-hidden border-2 border-black bg-white bg-gradient-radial from-yellow-300 to-orange-500 shadow-panel"
+    class="panel relative m-[1vmin] inline-block h-52 flex-auto basis-52 overflow-hidden border-2 border-black bg-white bg-gradient-radial shadow-panel"
+    :class="panelColor"
   >
     <slot></slot>
   </div>
 </template>
 
 <script lang="ts" setup>
-defineProps({
-  color: { type: String, default: 'white' },
-  bottom: Boolean
+import { computed } from 'vue';
+
+const props = defineProps({
+  color: { type: String, default: 'white' }
+});
+
+const panelColor = computed(() => {
+  switch (props.color) {
+    case 'blue':
+      return 'from-sky-300 to-sky-600';
+    case 'red':
+      return 'from-red-300 to-red-500';
+    case 'green':
+      return 'from-green-300 to-green-500';
+    case 'yellow':
+      return 'from-yellow-300 to-orange-500';
+    case 'purple':
+      return 'from-purple-300 to-purple-500';
+    case 'pink':
+      return 'from-pink-300 to-pink-500';
+    case 'gray':
+      return 'from-gray-300 to-gray-500';
+    default:
+      return 'from-white to-gray-300';
+  }
 });
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,25 @@
 <template>
   <header>
-    <div class="super-header">Astonishing Tales from the Astral Sea</div>
-    <h1>Weird Future</h1>
+    <div class="text-center font-tall-deco text-5xl">Astonishing Tales from the Astral Sea</div>
+    <h1 class="">Weird Future</h1>
   </header>
   <main>
     <section class="flex flex-wrap p-[1vmin] font-comic">
-      <HomePanel>
-        <HomePanelText :top="true"> Suddenly... </HomePanelText>
-        <HomePanelText :bottom="true">...something happened</HomePanelText>
+      <HomePanel color="blue">
+        <HomePanelText top> Suddenly... </HomePanelText>
+        <HomePanelText bottom>...something happened</HomePanelText>
       </HomePanel>
-      <HomePanel>
-        <HomePanelText :top="true">Try resizing...</HomePanelText>
-        <HomePanelText :bottom="true">...it's responsive</HomePanelText>
+      <HomePanel color="red">
+        <HomePanelText top>Try resizing...</HomePanelText>
+        <HomePanelText bottom>...it's responsive</HomePanelText>
       </HomePanel>
-      <HomePanel>
-        <p class="speech">A speech bubble</p>
+      <HomePanel color="green">
+        <HomeSpeechBubble>A speech bubble</HomeSpeechBubble>
       </HomePanel>
-      <HomePanel></HomePanel>
-      <HomePanel></HomePanel>
-      <HomePanel></HomePanel>
-      <HomePanel></HomePanel>
+      <HomePanel color="yellow"></HomePanel>
+      <HomePanel color="purple"></HomePanel>
+      <HomePanel color="pink"></HomePanel>
+      <HomePanel color="gray"></HomePanel>
       <HomePanel></HomePanel>
       <HomePanel>
         <HomePanelText :bottom="true">THE END</HomePanelText>
@@ -31,4 +31,5 @@
 <script setup lang="ts">
 import HomePanel from '@/components/HomePanel.vue';
 import HomePanelText from '@/components/HomePanelText.vue';
+import HomeSpeechBubble from '@/components/HomeSpeechBubble.vue';
 </script>
